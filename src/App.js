a1d20fe07dacd76b90fc077c2d1db6b64a199670
nmkhedekar <nmkhedekar@virtualemployee.com>
2023-03-09 12:44:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { useEffect } from 'react';
 
 function App() {
+  
+  const initApi = async () => {
+    const { data } = await axios.get("http://127.0.0.1:5000");
+    console.log("initApi", data);
+  }
+
+  useEffect(() => {
+    initApi();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Gecko Board
       </header>
     </div>
   );
